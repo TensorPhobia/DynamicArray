6e4871df5e7695070cf60f22d3e3f3b57821035d
TensorPhobia <157731400+TensorPhobia@users.noreply.github.com>
2025-05-27 22:06:36 +0100

    Update DynamicArray.c

--- DynamicArray.c
+++ DynamicArray.c
@@ -2,12 +2,6 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-unsigned long logBase(unsigned long base ,unsigned long number){
-    return ceil(log(number)/log(base));
-}
 
 DynamicArray DynamicArray_constructor(unsigned long capacity){
     DynamicArray arr;
@@ -23,32 +17,31 @@ void DynamicArray_destructor(DynamicArray *arr){
     free(arr->raw);
 }
 
-int DynamicArray_overflow_handler(DynamicArray *arr , float growth_rate){
-    assert(growth_rate > 1);
-    int newCap = arr->cap * growth_rate;
-    void * newMem = realloc(arr->raw,newCap);
+int DynamicArray_overflow_handler(DynamicArray *arr , unsigned long new_capacity){
+    assert(new_capacity > 1);
+    void * newMem = realloc(arr->raw,new_capacity * sizeof(arr->raw[0]));
     if(!newMem) return REALLOC_FAILURE;
-    arr->cap = newCap;
+    arr->cap = new_capacity;
+    arr->raw = newMem;
     return REALLOC_SUCESS;
 }
 
 /*
-    data_len = 100
-    capacity = 10
-    Growth_rate = 2
-    capacity * Growth_rate ^ x >= data_len 
-    Growth_rate ^ x = data_len / capacity
-    we solve for x : x = LogBase(Growth_rate,data_len / capacity); 
-
+    Scaling Formula Used
+    new ArrCapacity = ArrCapacity  * GrowthRate ^ x
+    such as new ArrCapacity >= DataLen +  ArrDataLen
+    
 */
 
 void DynamicArray_pushback(DynamicArray *arr , char *data , unsigned long data_len){
     assert(data);
-    if(arr->cap - arr->len > data_len ){
+    unsigned long total_len = arr->len + data_len;
+    if(arr->cap  >= total_len ){
         memcpy(arr->raw + arr->len, data, sizeof(data[0]) * data_len);
     }else{
-        unsigned long x = logBase(GROWTH_RATE, data_len / arr->cap);
-        assert(DynamicArray_overflow_handler(arr , pow(GROWTH_RATE,x)) == REALLOC_SUCESS);
+        unsigned long new_capacity = arr->cap == 0? 1 : arr->cap;
+        while(new_capacity <= total_len)  new_capacity*=GROWTH_RATE;
+        assert(DynamicArray_overflow_handler(arr ,new_capacity ) == REALLOC_SUCESS);
         memcpy(arr->raw + arr->len, data, sizeof(data[0]) * data_len);
     }
     arr->len+=data_len;
