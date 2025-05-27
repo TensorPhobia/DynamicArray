47a3c1919cda1aa00bd7d1b56e6c33e0ca2d2ece
TensorPhobia <157731400+TensorPhobia@users.noreply.github.com>
2025-05-27 22:04:10 +0100

    Update test_dynamic_array.c

--- test_dynamic_array.c
+++ test_dynamic_array.c
@@ -1,17 +1,13 @@
 #include "./DynamicArray.h"
 #include <stdio.h>
-#include <string.h>
 
 
 int main(){
-    DynamicArray arr = DynamicArray_constructor(3);
-    printf("len: %lu cap: %lu\n",arr.len,arr.cap);
-    char long_str[] = "The quick brown fox jumps over the lazy dog";
-    DynamicArray_pushback(&arr,long_str , strlen(long_str));
-    printf("len: %lu cap: %lu\n",arr.len,arr.cap);
-    for (int  i = 0; i  < arr.len; i++) {
-        printf("%c",arr.raw[i]);
+    DynamicArray arr = DynamicArray_constructor(0);
+    for (char i = 'A'; i <= 'z'; i++) {
+        printf("content: %c , capacity: %lu\n",i,arr.cap);
+        DynamicArray_pushback(&arr, &i , 1);
     }
-    puts("");
+    printf("content: %c , capacity: %lu\n",'z',arr.cap);
     return 0;
 }   
